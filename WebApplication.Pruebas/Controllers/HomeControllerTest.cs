c854662662c346a12c065e72e3e5505bca201f10
Luigi Mendoza <luigi.mendoza@bit2bitamericas.com>
2020-04-22 22:31:44 -0500

    first commit

---
+++ WebApplication.Pruebas/Controllers/HomeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace WebApplication.Pruebas.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void TestIndexReturnView()
+        {            
+            var controller = new HomeController();
+            var view = controller.Index() as ViewResult;
+            Assert.IsInstanceOf<ViewResult>(view);
+            Assert.IsInstanceOf<List<User>>(view.Model);
+        }
+
+        [Test]
+        public void TestAboutReturnView()
+        {
+            var controller = new HomeController();
+            var view = controller.About() as ViewResult;
+
+            Assert.IsNotNull(view.ViewBag.Message);
+            Assert.AreEqual("Mi Valor", view.ViewBag.Valor);
+            Assert.IsInstanceOf<ViewResult>(view);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // return lista de usuarios
+            var users = GetUsuarios();
+            return View(users);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Valor = "Mi Valor";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Guardar(User user)
+        {
+            if (user == null)
+            {
+                ViewBag.Message = "Usuario no puede ser null";
+                return View(user);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private List<User> GetUsuarios()
+        {
+            return new List<User>();
+        }
+    }
+}
